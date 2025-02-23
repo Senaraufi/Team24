cde4f8f940667cfe81b9828525e157900092edea
sena <88288148+Senaraufi@users.noreply.github.com>
2025-02-23 07:47:59 +0000

    compiler errors fixed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
